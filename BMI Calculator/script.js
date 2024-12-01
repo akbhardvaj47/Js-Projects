bb1576fe4ca3abe8c2200b065f5c193193022d7a
Amit Kumar Bhardvaj <amitkumarbhardwaj030@gmail.com>
2024-12-01 15:00:23 +0530

    Delete BMI Calculator directory

--- BMI Calculator/script.js
+++
@@ -1,7 +0,0 @@
-function bmi(){
-    var h=document.getElementById("height").value;
-    var w=document.getElementById("weight").value;
-    var total= w/(h/100*h/100);
-    total=total.toFixed(2);
-    document.getElementById("result").innerHTML="Your BMI is "+ total;
-}
